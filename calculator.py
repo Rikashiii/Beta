0d17c76519ff0141af25f3043fe40b09476958ba
Harsh Chandrakar <harshtg9999@gmail.com>
2023-03-03 21:14:04 +0530

    Has few bugs
    The calculations seems incorrect if we try addition the second time. See if you can debug it...

---
+++ calculator.py
@@ -0,0 +1,36 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+#dictionary to store functions
+dict1 = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+def calculator():
+  one_moreask = "y"
+  num1 = int(input("Enter the first number:"))
+  for i in dict1:
+      print(i)
+  while one_moreask == "y":
+      opr_symb = input("Pick an operation: ")
+      num2 = int(input("Enter the second number:"))
+      opr_func = dict1[opr_symb]
+      answer = opr_func(num1, num2)
+      print(answer)
+      one_moreask = input(
+          f"type 'y' to continue calculating with {answer}, or type 'n' to exit: "
+      )
+  else:
+      one_moreask == "n"
+      calculator()
+calculator()
